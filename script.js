fc7e50e2bddfdc6736ea35f3a4f357a582c09ee7
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2020-06-22 17:21:16 -0300

    Função que cria a div do jogador de acordo com o jogador atual

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// define o primeiro jogador (1/2);
 // define a estrutura 7x6;
 
 // função que cria a tabela com 7 colunas e 6 linhas:
@@ -6,10 +5,31 @@
     // cada liniha precisa receber a classe correspondente no css;
     // cada célula precisa receber a classe correspondente no css;
 
+// define o primeiro jogador (1/2);
+let jogadorAtual = 1;
+const classJogadorUm = "celula jogador1";
+const classJogadorDois = "celula jogador2"
+
 // função que cria a div do jogador 1 ou 2:
-    // inicia com o jogador atual;
+    // o primeiro parâmetro tem que ser sempre o jogadorAtual, e o "lugar" é a div onde precisa fazer o appendChild
+    
+// inicia com o jogador atual;
     // cria a div correspondente à classe;
     // altera para o próximo jogador;
+const criaDivJogador = (jogador, lugar) => {
+
+    let criaDiv = document.createElement('div')
+
+    if (jogador === 1) {
+        criaDiv.className = classJogadorUm;
+        lugar.appendChild(criaDiv)
+        jogadorAtual = 2;
+    } else {
+        criaDiv.className = classJogadorDois;
+        lugar.appendChild(criaDiv);
+        jogadorAtual = 1;
+    }
+}
 
 // função que verifica a a posição de inserção da peça do player:
     // chama a função de criação do jogador;
@@ -25,7 +45,7 @@
         // cria a div do resultado;
         // atribui a classe correspondente;
         // apensa a div no lugar dela no html;
-    
+
     // se houver empate:
         // cria div do resultado;
         // atribui a classe correspondente;
