b91d6bb1839268850b6bb80b7be6e0bd76228ca4
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-06-23 15:43:32 -0300

    - Inserido função que verifica condição de vitória!

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ const tabuleiro = [
     [0, 0, 0, 0, 0, 0, 0],
 ]
 
+// Inicia as funções para criar as divs com as suas classes.
 function initTabuleiro() {
     createElementsColuna("coluna");
     createElementsCelula("celula");
 }
 
-
+// Criação das divs colunas, loop de criação de 7 colunas.
 function createElementsColuna(className) {
     for(let k = 0; k < 7; k++){
         const celula = document.createElement('div');
@@ -23,6 +24,7 @@ function createElementsColuna(className) {
     } return;
 }
 
+// Criação das divs celulas dentro das divs colunas criadas, 6 celulas cada coluna.
 function createElementsCelula(className){
     let arrayColunas = document.querySelectorAll('.coluna');
     for(let i = 0; i < arrayColunas.length; i++){
@@ -33,7 +35,63 @@ function createElementsCelula(className){
         }
     }
 }
+
+function condicaoVitoria(){
+    // Encontrar se tem 4 iguais na HORIZONTAL:
+    const bordaX = (tabuleiro[0].length) - 3;
+    // loop nas linhas do tabuleiro:
+    for(let l = 0; l < tabuleiro.length; l++){
+        // linha em cada celula da linha:
+        for(let x = 0; x < bordaX; x++){
+            let celula = tabuleiro[l][x];
+            if(celula !== 0){
+                if(celula === tabuleiro[l][x+1] && celula === tabuleiro[l][x+2] && celula === tabuleiro[l][x+3]){
+                    // FIM do jogo 4 seguidos na horizontal.
+                }
+            }
+        } 
+    }
+    // Encontrar se tem 4 iguais na VERTICAL:
+    const bordaY = (tabuleiro.length) - 3;
+    for(let l = 0; l < bordaY; l++){
+        for(let x = 0; x < tabuleiro[0].length; x++){
+            const celula = tabuleiro[l][x];
+            if(celula !== 0){
+                if(celula === tabuleiro[l+1][x] && celula === tabuleiro[l+2][x] && celula === tabuleiro[l+3][x]){
+                    // FIM do jogo 4 seguidos na Vertical.
+                }
+            }
+        }
+    }
+    // Encontrar se tem 4 iguais na Diagonal esquerda p/ direita:
+    for(let l = 0; l < bordaY; l++){
+        for(let x = 0; x < bordaX; x++){
+            const celula = tabuleiro[l][x];
+            if(celula !== 0){
+                if(celula === tabuleiro[l+1][x+1] && celula === tabuleiro[l+2][x+2] && celula === tabuleiro[l+3][x+3]){
+                    // Fim de jogo 4 seguidos Diagonal esquerda p/ direita.
+                }
+
+            }
+        }
+    }
+    // Encontrar se tem 4 iguais na Diagonal direita p/ esquerda:
+    for(let l = 3; l < tabuleiro.length; l++){
+        for(let x = 0; x < bordaX; x++){
+            const celula = tabuleiro[l][x];
+            if(celula !== 0){
+                if(celula === tabuleiro[l-1][x+1] && celula === tabuleiro[l-2][x+2] && celula === tabuleiro[l-3][x+3]){
+                    // FIM de jogo 4 seguidos Diagonal Direita p/ esquerda.
+                }
+
+            }
+        }
+    }
+
+} 
+
 initTabuleiro();
+condicaoVitoria();
 
 
 // define o primeiro jogador (1/2);
