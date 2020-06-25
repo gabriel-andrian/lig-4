e79cadefc31afbb77a0969163c2716aeceaa8014
Gabriel Andrian <gabriel_andrian@hotmail.com>
2020-06-25 14:42:17 -0300

    - Limpeza dos comentários e inserido o .icon!

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const tabuleiro = [
     [0, 0, 0, 0, 0, 0, 0],
 ]
 
-// define o primeiro jogador (1/2);
+// Definir variaveis globais:
 let jogadorAtv = 1;
 let gameOver = false;
 let linha = 0;
@@ -19,9 +19,7 @@ let cont = 0;
 const classJogadorUm = "jogador1";
 const classJogadorDois = "jogador2";
 
-// função que cria a tabela com 7 colunas e 6 linhas:
-// cada liniha precisa receber a classe correspondente no css;
-// cada célula precisa receber a classe correspondente no css;
+// Função para inicializar o tabuleiro vazio:
 function initTabuleiro() {
     createElementsColuna("coluna");
     createElementsCelula("celula");
@@ -48,17 +46,14 @@ function createElementsCelula(className) {
         }
     }
 }
-
+// Função para verificiar se ouve alguma vitória ou empate:
 function condicaoVitoria() {
-    // Verifica se não foi empate:
     if(cont === 42){
         mensagemEmpate();
     }
     // Encontrar se tem 4 iguais na HORIZONTAL:
     const bordaX = (tabuleiro[0].length) - 3;
-    // loop nas linhas do tabuleiro:
     for (let l = 0; l < tabuleiro.length; l++) {
-        // linha em cada celula da linha:
         for (let x = 0; x < bordaX; x++) {
             let celula = tabuleiro[l][x];
             if(celula !== 0){
@@ -106,8 +101,8 @@ function condicaoVitoria() {
 
 }
 
+// Função para aparecer mensagem de vitória, botão reset e parar o jogo:
 function mensagemVitoria(){
-    // Aparece a mensagem de vitória com o botão de reset e quem ganhou.
     gameOver = true;
     const divVitoria = document.getElementById('vencedor');
     const mensagemVitoria = document.getElementById('msgVitoria')
@@ -118,9 +113,8 @@ function mensagemVitoria(){
         mensagemVitoria.textContent = "PARABÉNS O JOGADOR 1 GANHOU!!!";
     }
 }
-
+// Função para aparecer mensagem de empate, botão reset e parar o jogo:
 function mensagemEmpate(){
-    // Aparece a mensagem de empate com o botão de reset.
     gameOver = true;
     const divVitoria = document.getElementById('vencedor');
     const mensagemVitoria = document.getElementById('msgVitoria')
@@ -129,41 +123,29 @@ function mensagemEmpate(){
 }
 
 // função que cria a div do jogador 1 ou 2:
-//// o primeiro parâmetro tem que ser sempre o jogadorAtual, e o "lugar" é a div onde precisa fazer o appendChild
 const criaDivJogador = () => {
-
-    // inicia com o jogador atual;
     if (jogadorAtv === 1) {
-        // cria a div correspondente à classe;
         let criaDiv = document.createElement('div')
-        // altera para o próximo jogador;
         jogadorAtv = 2;
         criaDiv.className = classJogadorUm;
         return criaDiv;
     } else {
-        // cria a div correspondente à classe;
         let criaDiv = document.createElement('div')
         criaDiv.className = classJogadorDois;
-        // altera para o próximo jogador;
         jogadorAtv = 1;
         criaDiv.className = classJogadorDois;
         return criaDiv;
     }
 }
 
+// Função de jogada, verifica se a bolinha do jogador pode ser inserida (main do programa):
 function jogada(e) {
     if(gameOver !== true){
-        // Retorna true ou false
         let isCelula = e.target.classList.contains('celula');
-        let isColuna = e.target.classList.contains('coluna');
-    
-        // Retorna a quantidade de filhos da célula clicada
+        let isColuna = e.target.classList.contains('coluna');    
         let hasChild = e.target.childElementCount;
-    
-        // Captura a célula clicada
         let target = e.currentTarget;
     
-        // Modifica o número (index) da coluna
         coluna = parseInt(e.target.parentNode.id.split('').pop()) - 1;
     
         if (isCelula === true && hasChild === 0) {
@@ -188,43 +170,10 @@ function jogada(e) {
         }
     }
 }
-
-
-const atualizaPos = () => {
-    // cada index do array é uma coluna da tabela, -1
-    // cada index do subarray representa o numero da linha
-
-    let idColuna = document.getElementById("coluna")
-}
-
+// Inicializa a função do tabuleiro Inicial:
 initTabuleiro();
 
+// Verifica se aconteceu algum evento de clique nas colunas:
 for (let a = 1; a <= 7; a++) {
     document.getElementById(`coluna${a}`).addEventListener('click', jogada)
 }
-
-
-// função que verifica a condição de vitória/empate:
-    // 4 peças lado a lado na horizontal;
-    // 4 peças lado a lado na vertical;
-    // 4 peças lado a lado na diagona da esquerda-acima para direita-abaixo;
-    // 4 peças lado a lado na diagona da direita-acima para a esquerda-abaixo;
-
-// função que imprime o resultado:
-    // se houver vitória:
-        // cria a div do resultado;
-        // atribui a classe correspondente;
-        // apensa a div no lugar dela no html;
-
-    // se houver empate:
-        // cria div do resultado;
-        // atribui a classe correspondente;
-        // apensa a div no lugar dela no html
-
-//evento de clique inserido na coluna:
-    // pega o id da linha e o último filho;
-    // chama a função que verifica a posição de inserção;
-    // chama função que verifica condição de vitória;
-        // se houver vitória/empate:
-            // chama função que imprime o resultado;
-        // senão chama o jogador seguinte
